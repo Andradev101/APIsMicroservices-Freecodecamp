5d357a86e11d32ebd7bb06ba596d630bb8cef333
Luiz Henrique Santos de Andrade <71150787+Andradev101@users.noreply.github.com>
2021-07-28 19:12:18 +0000

    Lesson 6/12

--- myApp.js
+++ myApp.js
@@ -1,6 +1,25 @@
 var express = require('express');
 var app = express();
 
+const mySecret = process.env['MESSAGE_STYLE']
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+//creating / route, which sends a file [...]/views/index.html
+
+app.use("/public", express.static(__dirname + "/public"));
+//use the files inside the directory, statically
+
+app.get("/json", (req, res) => {
+  if(process.env.MESSAGE_STYLE == "uppercase"){
+    res.json({message:"Hello json".toUpperCase()});
+  }else{
+    res.json({message:"Hello json"});
+  }
+})
+//creates /json route, and if MESSA_STYLE equals uppercase, it return HELLO JSON, if not returns Hello json
+
 
 
 
